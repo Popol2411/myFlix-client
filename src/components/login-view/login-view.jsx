c6aae43e7029097fd11c1ba67dd9a7949ae3ef0a
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-03-29 17:58:57 +0200

    comments added

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export function LoginView(props) {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState('');   //useState is a "Hook" from React. In contrast to a class, functions with hooks don´t need to use the ".this.state". Instead we use "useState" directly  
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault();   //preventDefault page, supresses the refresh of the page
     console.log(username, password);
     props.onLoggedIn(username);
   };
+
+  //a form with username, password is being returned to the user in order to submit their info
+  // "e" for "event"
 
   return (
     <form>
