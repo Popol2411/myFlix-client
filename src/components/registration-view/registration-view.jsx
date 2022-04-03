f89ddac4d271f4e8dcfdce98296e1e1f24a812d9
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-04-03 19:26:04 +0200

    typo mistakes corrected

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -29,17 +29,17 @@ export function RegistrationView(props) {
     if (!username) {
       setValues({ ...values, usernameErr: 'Username required' });
       isReq = false;
-    } else if {
-      setValues({ ...values, usernameErr: 'Username must be 5 characters long'})
+    } else if (username.length < 5) {
+      setValues({ ...values, usernameErr: 'Username must be 5 characters long' })
       isReq = false;
-    }  
-     if (!password) {
-        setValues({ ...values, passwordErr: 'Password Required' });
-        isReq = false;
-      } else if (password.length < 6) {
-        setValues({ ...values, passwordErr: 'Password must be 6 characters long' })
-        isReq = false;
-      }
+    }
+    if (!password) {
+      setValues({ ...values, passwordErr: 'Password Required' });
+      isReq = false;
+    } else if (password.length < 6) {
+      setValues({ ...values, passwordErr: 'Password must be 6 characters long' })
+      isReq = false;
+    }
     if (!email) {
       setValues({ ...values, emailErr: 'Email Required' });
       isReq = false;
@@ -86,14 +86,14 @@ export function RegistrationView(props) {
             <Form.Label>Username:</Form.Label>
             <Form.Control type="text" value={username} onChange={e =>
               setUsername(e.target.value)} />
-            {values.usernameErr && <p>{values.usernameErr</p>}
+            {values.usernameErr && <p>{values.usernameErr}</p>}
           </Form.Group>
 
           <Form.Group controlId="formName" className="reg-form-inputs">
             <Form.Label>Name:</Form.Label>
             <Form.Control type="text" value={name} onChange={e =>
               setName(e.target.value)} />
-            {values.nameErr && <p>{values.nameErr</p>}
+            {values.nameErr && <p>{values.nameErr}</p>}
           </Form.Group>
 
 
@@ -101,14 +101,14 @@ export function RegistrationView(props) {
             <Form.Label>Password:</Form.Label>
             <Form.Control type="password" value={password} onChange={e =>
               setPassword(e.target.value)} />
-            {values.passwordErr && <p>{values.passwordErr</p>}
+            {values.passwordErr && <p>{values.passwordErr}</p>}
           </Form.Group>
 
           <Form.Group controlId="Email" className="reg-form-inputs">
             <Form.Label>Email:</Form.Label>
             <Form.Control type="email" value={password} onChange={e =>
               setEmail(e.target.value)} />
-            {values.emailErr && <p>{values.emailErr</p>}
+            {values.emailErr && <p>{values.emailErr}</p>}
           </Form.Group>
 
 
