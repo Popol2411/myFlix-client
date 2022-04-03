d965b96f8a50dbf8cf2d30e322e6bcd7f2972099
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-04-03 09:15:04 +0200

    updated validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -7,41 +7,70 @@ export function LoginView(props) {
   const [username, setUsername] = useState('');   //useState is a "Hook" from React. In contrast to a class, functions with hooks don´t need to use the ".this.state". Instead we use "useState" directly  
   const [password, setPassword] = useState('');
 
+  // Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr('Username Required');
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr('Username must be 2 characters long');
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr('Password Required');
+      isReq = false;
+    } else if (password.length < 6) {
+      setPassword('Password must be 6 characters long');
+      isReq = false;
+    }
+
+    return isReq;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    /* Send a request to the server for authentication */
-    axios.post('https://myflixdbpopol.herokuapp.com/login', {     //API Login Url
-      Username: username,
-      Password: password
-    })
-      .then(response => {
-        const data = response.data;
-        props.onLoggedIn(data);
+    const isReq = validate();
+    if (isReq) {
+      /* Send request to the server for authentication */
+      axios.post('https://myflixdbpopol.herokuapp.com/login', {
+        Username: username,
+        Password: password
       })
-      .catch(e => {
-        console.log('no such user')
-      });
+        .then(response => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch(e => {
+          console.log('no such user')
+        });
+    }
   };
-
-  //a form with username, password is being returned to the user in order to submit their info
-  // "e" for "event"
 
   return (
     <Form>
       <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+        {/* code added here to display validation error */}
+        {usernameErr && <p>{usernameErr}</p>}
       </Form.Group>
 
       <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        {/* code added here to display validation error */}
+        {passwordErr && <p>{passwordErr}</p>}
       </Form.Group>
       <Button variant="primary" type="submit" onClick={handleSubmit}>
         Submit
       </Button>
     </Form>
-  );
+  )
 }
 
 LoginView.propTypes = {
