0d298a804690614c4c98e8e0da1a11f743a93910
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-07-14 22:17:21 +0200

    Link dublette in import removed

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';  //as for the main-view.jsx, react needs to be imported in order to use the react library.
 import PropTypes from 'prop-types';   //    //since we use prop-types (Property Types), we need to import these too.
-import { Container, Row, Col, Card, Button, Link } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
